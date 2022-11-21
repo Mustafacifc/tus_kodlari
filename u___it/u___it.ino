a789b904bdd6c4f68d463ddec17ba47574f050aa
Mustafacifc <114651181+Mustafacifc@users.noreply.github.com>
2022-11-21 10:31:20 +0300

    tus_kodlari

---
+++ u___it/u___it.ino
@@ -0,0 +1,15 @@
+int girilen_deger=0;
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+if(Serial.available()){
+girilen_deger=Serial.read();
+Serial.print("GirilenDeğer:");
+Serial.print(girilen_deger);
+Serial.print(" - Char Olarak Girilen Değer: ");
+Serial.println( (char)girilen_deger );
+}
+}
